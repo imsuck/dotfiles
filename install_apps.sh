606f7f4ed79bcf4da6c099718fcb5a8947a8a01d
imsuck <imsuck12@gmail.com>
2023-02-01 15:37:48 +0700

    2023-02-01_15-36-54 1. Move fonts around 2. Edit install_apps a bit

---
+++ install_apps.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/sh
+
+# use `#package` when needed to remove undesired packages
+#
+# helix macro: "mi<S-W>ms`<A-;>;a#<esc>"
+# helix macro to undo: "md`<S-F>#;d"
+# Use `"@y` to yank macro ok
+
+sudo pacman -S --noconfirm --needed \
+ alacritty wezterm starship helix `#dunst` github-cli fzf zellij\
+ exa ripgrep fd bat zoxide `# rust utilities`\
+ htop bottom\
+ discord opera nitrogen\
+ `#polybar` rofi\
+ xclip shotgun hacksaw `# screenshot`\
+ ttf-fira-code ttf-font-awesome ttf-hack `#otf-ipafont` ttf-iosevka-nerd ttf-roboto-mono\
+ xf86-input-synaptics\
+ rustup rust-analyzer bacon `# rust dev things`\
+ cronie stalonetray xdo ranger\
+ # thefuck
+ # picom
+
+yay -S --answerclean All --answerdiff None \
+ discocss-git\
+ opera-ffmpeg-codecs eww\
+ spotify-adblock-git cava\
+ nerd-fonts-hack betterlockscreen\
+ ksuperkey autotiling\
+ `#nerd-fonts-jetbrains-mono` ttf-icomoon-feather\
+ ksuperkey `#diskonaut` polybar-git picom-git polybar-spotify-module
+
+systemctl --user enable spotify-listener
+systemctl --user start spotify-listener
