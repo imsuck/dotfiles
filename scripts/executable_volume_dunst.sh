9946e2beafea8910838b727ea053a68a1270120b
imsuck <imsuck12@gmail.com>
2023-11-11 22:45:12 +0700

    i3: Volume and brightness shortcuts

---
+++ scripts/executable_volume_dunst.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+send_notification() {
+	volume=$(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))
+	notify-send -u low -r 9993 -h int:value:"$volume" -i "volume-$1" "Volume: ${volume}" -t 2000
+}
+
+case $1 in
+up)
+	amixer --quiet -D pulse sset Master unmute
+	amixer --quiet -D pulse sset Master 5%+
+	send_notification "$1"
+	;;
+down)
+	amixer --quiet -D pulse sset Master unmute
+	amixer --quiet -D pulse sset Master 5%-
+	send_notification "$1"
+	;;
+mute)
+	amixer --quiet -D pulse sset Master toggle
+	if [[ -z "$(amixer -D pulse get Master | rg '\[on\]')" ]]; then
+		dunstify -a "changevolume" -t 2000 -r 9993 -u low -i "volume-mute" "Muted"
+	else
+		send_notification up
+	fi
+	;;
+esac
