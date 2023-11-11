27a0d06da892da15bb4a1bbfaac8bb9a70135c20
imsuck <imsuck12@gmail.com>
2023-11-11 22:45:12 +0700

    i3: Volume and brightness shortcuts

---
+++ scripts/executable_brightness_dunst.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+send_notification() {
+	brightness=$(printf "%.0f\n" "$(brillo -G)")
+	dunstify -u low -r 9991 -h int:value:"$brightness" -i "sun" "Brightness: $brightness%" -t 2000
+}
+
+case $1 in
+up)
+	brillo -A 2.5 -p
+	send_notification "$1"
+	;;
+down)
+	brillo -U 2.5 -p
+	send_notification "$1"
+	;;
+esac
