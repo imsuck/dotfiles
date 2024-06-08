fdc8fcc4eb86a4db664e70daf51dd57e9eaee409
imsuck <imsuck12@gmail.com>
2024-06-08 10:37:57 +0700

    Update helix configs, zellij scripts, fix screenshots - helix: Disable inlay hints, update C++ formatting - zellij: Update auto compile script - scripts: Fix screenshots saving empty files

--- scripts/executable_screenshot.sh
+++ scripts/executable_screenshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-selection=$(hacksaw)
+selection=$(hacksaw) || exit 1
 name="$HOME/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png"
 shotgun -g "$selection" - > $name
 xclip -t "image/png" -selection clipboard $name
