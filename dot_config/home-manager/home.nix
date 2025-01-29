3b69506a87c1ed5a366db5bd8969b33ea8c1441c
imsuck <imsuck12@gmail.com>
2025-01-29 23:18:01 +0700

    opentabletdriver didn't work with home-manager

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -13,48 +13,19 @@
     bottom btop htop
     neovim
     nix-tree nix-web
-    opentabletdriver
     typst
     zellij
   ];
 
   home.file = {};
 
-  xdg.configFile = {
-    "systemd/user/opentabletdriver.service".text = ''
-      [Unit]
-      Description=OpenTabletDriver Daemon
-      PartOf=graphical-session.target
-      After=graphical-session.target
-      ConditionEnvironment=|WAYLAND_DISPLAY
-      ConditionEnvironment=|DISPLAY
-
-      [Service]
-      ExecStart=${pkgs.opentabletdriver}/bin/otd-daemon
-      Restart=always
-      RestartSec=3
-
-      [Install]
-      WantedBy=graphical-session.target
-    '';
-  };
+  xdg.configFile = {};
 
   home.sessionVariables = {
     # EDITOR = "hx";
   };
 
-  home.activation = {
-    enableOTDDaemon = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      # very bad!!!
-      _ctl="/usr/bin/systemctl"
-      if command -v $_ctl 2>&1 >/dev/null; then
-        $_ctl enable --user --now opentabletdriver
-        $_ctl --user daemon-reload
-      else
-        printf "\e[31mFailed to reload opentabletdriver daemon\e[0m\n"
-      fi
-    '';
-  };
+  home.activation = {};
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
