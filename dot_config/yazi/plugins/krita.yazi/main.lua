8dd202fbcde3caaa24db5d5010d77ff90e14b625
imsuck <imsuck12@gmail.com>
2025-04-29 21:32:48 +0700

    IDK

---
+++ dot_config/yazi/plugins/krita.yazi/main.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+function M:peek(job)
+  local start, cache = os.clock(), ya.file_cache(job)
+  if not cache then
+    return
+  end
+
+  local ok, err = self:preload(job)
+  if not ok or err then
+    return
+  end
+
+  ya.sleep(math.max(0, rt.preview.image_delay / 1000 + start - os.clock()))
+  ya.image_show(cache, job.area)
+  ya.preview_widgets(job, {})
+end
+
+function M:preload(job)
+  local cache = ya.file_cache(job)
+  if not cache or fs.cha(cache) then
+    return true
+  end
+
+  local output, err = Command("unzip")
+    :args({
+      "-p",
+      tostring(job.file.url),
+      "preview.png",
+    })
+    :stdout(Command.PIPED)
+    :output()
+
+  if not output then
+    return true, Err("Failed to start `unzip`, error: %s", err)
+  elseif not output.status.success then
+    return true, Err("Failed to unzip .kra file, stderr: %s", output.stderr)
+  end
+
+  return fs.write(cache, output.stdout)
+end
+
+return M
--- install_packages.sh
+++ install_packages.sh
@@ -6,17 +6,22 @@
 # helix macro to undo: "md`<S-F>#;d"
 # Use `"@y` to yank macro ok
 
-sudo pacman -S --noconfirm --needed \
- bat bat-extras fzf \
- clang typst tinymist watchexec zellij \
- alacritty wezterm bottom `#dunst` \
- firefox nitrogen handlr ripdrag \
- xclip shotgun hacksaw `# screenshot` \
- rustup rust-analyzer bacon sccache `# rust dev things` \
- cronie `#stalonetray` xdo `#ranger` \
- fcitx5-bamboo fcitx5-mozc-ut ibus `# needed for fcitx5 (???)` \
- polybar picom rofi rofi-emoji
- # thefuck
+alias pac_inst 'sudo pacman -S --noconfirm --needed'
+
+pac_inst bat bat-extras dust eza fd fzf ripdrag starship yazi
+# dev
+pac_inst clang helix mise tinymist typst watchexec zellij
+# rust dev
+pac_inst rustup rust-analyzer bacon sccache
+
+pac_inst alacritty bottom handlr wezterm
+pac_inst firefox nitrogen picom polybar qalculate-gtk rofi rofi-emoji zathura
+
+# screenshot
+pac_inst xclip shotgun hacksaw
+
+pac_inst xdo `#cronie` `#stalonetray`
+pac_inst fcitx5-bamboo fcitx5-mozc-ut ibus # needed for fcitx5 (???)
 
 yay -S --answerclean All --answerdiff None \
  autotiling ksuperkey \
