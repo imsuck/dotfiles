b66fe443bbe0233ff159efc9bcf4c6ffd93c4015
imsuck <imsuck12@gmail.com>
2025-05-05 21:39:58 +0700

    yazi plugins stuff

--- plugins.fish
+++ plugins.fish
@@ -3,3 +3,6 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/1f0dc2b4970da160605638cb0f157079660d6e04/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install edc/bass
 fisher install jorgebucaran/autopair.fish
+
+# yazi plugins, don't ask why I put it here
+ya pack -u
