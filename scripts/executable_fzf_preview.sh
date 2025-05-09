23f5fc7f15601e0bfaa6399bf48c797cca2fbd4f
imsuck <imsuck12@gmail.com>
2025-05-09 14:44:00 +0700

    Change starship prompt

--- scripts/executable_fzf_preview.sh
+++ scripts/executable_fzf_preview.sh
@@ -3,7 +3,7 @@
 main() {
   COLUMNS=$FZF_PREVIEW_COLUMNS
   if [[ -d $1 ]]; then
-    exa -G --icons --color always $1
+    eza -l --icons --color always $1
   else
     bat --theme ansi --style 'changes,grid,numbers' -f $1 | \
       tail -n +2 | head -n -1
