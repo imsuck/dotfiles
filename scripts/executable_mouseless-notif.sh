5bc425c64eff471cfe99fe4601c52f19a45336de
imsuck <imsuck12@gmail.com>
2026-09-04 15:11:41 +0700

    fix mouseless notif for KDE

--- scripts/executable_mouseless-notif.sh
+++ scripts/executable_mouseless-notif.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env sh
 
 PIDFILE="/tmp/mouseless-watchexec.pid"
+STATEFILE="/var/tmp/mouseless.state"
 
 if [ -f "$PIDFILE" ] && kill -0 "$(cat "$PIDFILE")" 2>/dev/null; then
   kill "$(cat "$PIDFILE")"
   rm -f "$PIDFILE"
 fi
 
-watchexec -pw /var/tmp/mouseless.state \
-  'notify-send -r 32835 -t 500 "$(cat /var/tmp/mouseless.state)"' &
+case "${XDG_CURRENT_DESKTOP:-}" in
+  *KDE*|*kde*|*PLASMA*|*plasma*)
+    NOTIFY_CMD='notify-send -t 500 "$(cat /var/tmp/mouseless.state)"'
+    ;;
+  *)
+    NOTIFY_CMD='notify-send -r 32835 -t 500 "$(cat /var/tmp/mouseless.state)"'
+    ;;
+esac
+
+watchexec -pw "$STATEFILE" "$NOTIFY_CMD" &
 
 echo $! > "$PIDFILE"
 
-# keep script alive so watchexec actually works
 while true; do
   sleep 86400
 done
